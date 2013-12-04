e9e63011df02258f5d43e312de39f30a887fd9c5
Marcel Richter <marcel.richter@gmail.com>
2013-12-04 22:56:58 +0100

    source and target added

--- jats-js/src/test/ts/demo/project/project.ts
+++ jats-js/src/test/ts/demo/project/project.ts
@@ -1,6 +1,6 @@
 ///<reference path='../model/Uom.ts' />
 ///<reference path='../model/Quantity.ts' />
 ///<reference path='../model/Product.ts' />
-///<reference path='../model/ModifierEntry.ts' />
 ///<reference path='../model/Modifier.ts' />
+///<reference path='../model/ModifierEntry.ts' />
 ///<reference path='../model/Component.ts' />
